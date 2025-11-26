d924764ce84db57a7d81c2809a853d5c0854b546
SatyamPant <satyampant420@gmail.com>
2025-11-26 16:54:28 +0530

    Data validation implemented

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -10,8 +11,15 @@ if __name__=="__main__":
         trainingpipelineconfig = TrainingPipelineConfig()
         data_ingestion_config=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(data_ingestion_config)
-
+        logging.info("Initiate the data ingestion component")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion is completed and artifact is created")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation component")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation is completed and artifact is created")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
